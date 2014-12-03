abd756665c45b0329beb70b2ae3166c9d3d9a763
reticentfat <wy07011002@gmail.com>
2014-12-03 09:36:55 +0800

    Create guangdong_foshan.sh
    媒体月详单

---
+++ guangdong_foshan.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+##每周一执行 15 0 * * 1 sh /home/oracle/etl/bin/guangdong_foshan.sh
+
+DEALDATE=`date '+%Y%m%d'`
+echo $DEALDATE
+
+ftp -i -v -n 211.139.201.152 <<EOF
+user czxms fs123456
+lcd /home/oracle/etl/data/guangdong/foshan/
+put ${DEALDATE}_foshan_wzcx_data_cancel.txt
+EOF
