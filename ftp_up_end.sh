8612b2a07ce8e8e7412fcc1555b555dda1e8b334
reticentfat <wy07011002@gmail.com>
2015-04-10 16:45:47 +0800

    Create ftp_up_end.sh

---
+++ ftp_up_end.sh
@@ -0,0 +1,14 @@
+DATE_FILE=`date +%Y%m`
+FILE_NAME=guangdong_qianxiang_nco_bgn.$DATE_FILE.txt
+DATE_DIR_FTP=`date +%Y-%m-04`
+GDQX_DIR="/data/wuying/zhangfang/guangdong_qx"
+
+
+ftp -v -n 218.206.87.169 << EOF
+user guangdong gmcc888
+cd $DATE_DIR_FTP
+lcd $GDQX_DIR
+prompt
+put $FILE_NAME
+bye
+EOF
