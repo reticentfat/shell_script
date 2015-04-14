ff43e28f7dddf5745ff24c6ed30f965614e2d16e
reticentfat <wy07011002@gmail.com>
2015-04-14 11:40:12 +0800

    Update ftp_up_end.sh

--- ftp_up_end.sh
+++ ftp_up_end.sh
@@ -7,8 +7,11 @@ GDQX_DIR="/data/wuying/zhangfang/guangdong_qx"
 ftp -v -n 218.206.87.169 << EOF
 user guangdong gmcc888
 cd $DATE_DIR_FTP
-lcd $GDQX_DIR
-prompt
+lcd $GDQX_DIR 
+#lcd [ directory ] 更改 local host 的工作目录
+prompt 
+#更改交谈模式，若为 on 则在 mput 与 mget 时每做一个文件传输时均会询问
 put $FILE_NAME
+#将 local host 的文件送到 remote host
 bye
 EOF
