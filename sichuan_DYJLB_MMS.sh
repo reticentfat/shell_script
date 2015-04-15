69cb681e49ab6b06fce1bfe177b28e65d4432e07
reticentfat <wy07011002@gmail.com>
2015-04-15 13:43:11 +0800

    Update sichuan_DYJLB_MMS.sh

--- sichuan_DYJLB_MMS.sh
+++ sichuan_DYJLB_MMS.sh
@@ -7,7 +7,7 @@ GDQX_DIR="/data/wuying/wangyuan"
 awk -F'|' -v NODIST_DIR=/home/chensm/nodist.tsv 'BEGIN{print "用户号码|订购时间|退订时间|操作状态|操作渠道|地市";}{if(FILENAME == NODIST_DIR) nodist[$4] = $2;else if(FILENAME != NODIST_DIR && substr($1, 1, 7) in nodist)  print $1"|"$2"|"$3"|"$4"|"$5"|"nodist[substr($1, 1, 7)];}' /home/chensm/nodist.tsv /data/wuying/wangyuan/temp1_${DEALDATE}.txt | bzip2 > /data/wuying/wangyuan/sichuan_DYJLB_MMS_${DEALDATE}.txt.bz2
 
 ftp -v -n 218.206.87.169 << EOF
-user sichuan h4o2p6s6
+user sichuan h123
 cd $DATE_DIR_FTP
 lcd $GDQX_DIR
 prompt
