a10a1e345bdd4c433919609f4f9cd7909beba7a0
reticentfat <wy07011002@gmail.com>
2015-06-11 11:15:59 +0800

    Update 2012-至今九大业务留存新增退订数量统计.sql

--- 2012-至今九大业务留存新增退订数量统计.sql
+++ 2012-至今九大业务留存新增退订数量统计.sql
@@ -1,13 +1,13 @@
-select substr(t.deal_date, 0, 4),
-       t.oper_name,
+select substr(t.deal_date, 0, 4) 年份,
+       t.oper_name 业务名称,
        sum(case
              when substr(t.deal_date, 6, 5) = '01-01' THEN
               t.on_user_num
              ELSE
               0
-           END),
-       sum(t.new_user_num),
-       sum(t.td_user_num)
+           END) 在线用户数,
+       sum(t.new_user_num) 新增用户数,
+       sum(t.td_user_num) 退订用户数
   from tb_rpt_oper_day t, opt_code o
  where t.oper_code = o.opt_type
    and o.report_class = '1'
