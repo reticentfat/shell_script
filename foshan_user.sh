bfc73bb31f5a378aa19e8bba4bb8cecf6001c93d
reticentfat <wy07011002@gmail.com>
2015-08-28 11:09:11 +0800

    Update foshan_user.sh

--- foshan_user.sh
+++ foshan_user.sh
@@ -4,7 +4,10 @@ YT=`date -v-1d +%Y-%m-%d`
 TODAY=`date +%Y-%m-%d`
 
 
-echo 'use WZCX_DB ; select MOBILE_SN,ADDR_CODE,ADDR_NATION_CODE,CAR_NO,CAR_TYPE,CAR_SN,FEE_APP_CODE,MOBILE_TYPE from wzcx_sub where OPT_ADDR = "GUANGDONG" AND CP_ID = "FOSHAN" AND MOBILE_MODIFY_STATE = 3 into outfile "/data0/gateway/cron/ftplog2dcp/foshan_wzcx_user.txt" fields terminated by " "   LINES TERMINATED BY "\n";' |  mysql -uroot -p1234567890;
+#echo 'use wzcx_db ; select mobile_sn,addr_code,addr_nation_code,car_no,car_type,car_sn,fee_app_code,mobile_type from wzcx_sub where opt_addr = "guangdong" and cp_id = "foshan" and mobile_modify_state = 3 into outfile "/usr/local/www/cron/foshan_wzcx_user.txt" fields terminated by " "   lines terminated by "\n";' |  mysql -uroot -p010@bj12580umg2011;
+echo 'use WZCX_DB ; select MOBILE_SN,ADDR_CODE,ADDR_NATION_CODE,CAR_NO,CAR_TYPE,CAR_SN,FEE_APP_CODE,MOBILE_TYPE from wzcx_sub where OPT_ADDR = "GUANGDONG" AND CP_ID = "FOSHAN" AND MOBILE_MODIFY_STATE = 3 into outfile "/usr/local/www/cron/foshan_wzcx_user.txt" fields terminated by " "   LINES TERMINATED BY "\n";' |  mysql -h192.100.7.19 -ur_slave_db -pwzMIWdZ93n45d5NT;
+
+
 
 
 
@@ -21,8 +24,15 @@ mv foshan_wzcx_user.txt ./$DEALDATE/
 
 bzip2 $DEALDATE/foshan_wzcx_user.txt 
 
-#rm -rf /data0/gateway/cron/ftplog2dcp/$DEALDATE
-mv $DEALDATE /data0/12580/api/fosha_data/
+mv $DEALDATE /usr/local/www/interface/fs_wzcx/
+
+
+
+
+
+
+
+
 
 
 
