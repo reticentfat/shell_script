776f91e20d5e7488bce0eb082292c4a0ca1e0eff
reticentfat <reticentfat@users.noreply.github.com>
2016-04-05 17:02:46 +0800

    Update 2012至2015年无线业务包月用户数.sql

--- 2012至2015年无线业务包月用户数.sql
+++ 2012至2015年无线业务包月用户数.sql
@@ -25,3 +25,31 @@ select A.省份，A.大类，A.年份,sum(A.在线),sum(A.新增) from (select t
  group by A.省份，A.大类，A.年份
 --select * from tb_rpt_oper_day t
 --select * from opt_code o
+-------------电影俱乐部---------------
+select A.省份，A.大类，A.年份,sum(A.在线),sum(A.新增) from (select t.prov_name 省份,
+
+       o.optbigclass 大类,
+       substr(t.deal_date, 1, 4) 年份,
+       sum(t.on_user_num) 在线,
+       0 新增
+  from tb_rpt_oper_day t, opt_code o
+ where  substr(t.deal_date, 6, 9) = '01-01' 
+ and o.opt_type = t.oper_code
+ and t.deal_date>='2012-01-01'
+ and  t.deal_date<'2016-01-01'
+ and o.appcode in ('10301013','10511008')
+ group by t.prov_name, o.optbigclass, substr(t.deal_date, 1, 4)
+ union all
+ select t.prov_name 省份,
+
+       o.optbigclass 大类,
+       substr(t.deal_date, 1, 4) 年份,
+       0 在线,
+       sum(t.new_user_num) 新增
+  from tb_rpt_oper_day t, opt_code o
+ where  t.deal_date>='2012-01-01'
+ and  t.deal_date<'2016-01-01'
+ and o.opt_type = t.oper_code
+  and o.appcode in ('10301013','10511008')
+ group by t.prov_name, o.optbigclass, substr(t.deal_date, 1, 4))A
+ group by A.省份，A.大类，A.年份
