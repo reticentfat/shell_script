df6149472e6f07f5e8afb292af644e7652428ccc
reticentfat <reticentfat@users.noreply.github.com>
2016-06-13 12:25:56 +0800

    Update 每业务提取三名用户.sql

--- 每业务提取三名用户.sql
+++ 每业务提取三名用户.sql
@@ -1,8 +1,10 @@
-select b.mobile_sn, o.opt_cost
+ select b.mobile_sn, o.opt_cost
    from (select *
            from (select t.*,
                         row_number() over(partition by t.appcode order by t.mobile_sn desc) RN
-                   from new_wireless_subscription t)
+                   from new_wireless_subscription t 
+                   where t.mobile_sub_state=3
+                   and t.is_paused=1)
           where RN <= 3) b,
         opt_code o
   where b.appcode = o.appcode
