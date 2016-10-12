e99fe57dd123cced3c9371b5754cefe8f5e720ce
reticentfat <reticentfat@users.noreply.github.com>
2016-10-12 17:00:06 +0800

    Update 全局搜索.sql

--- 全局搜索.sql
+++ 全局搜索.sql
@@ -1,1 +1,5 @@
 grep -nsr "outputumessage_003_meiti" shbb/
+oracle@wreport:/data/homeoracle/etl/bin$ find . -maxdepth 2 -name "*.sh" |xargs grep "report.region.sj_buss_wireless_city.csv"
+./exportmonth_KPI.sh:iconv -f GB2312 -t UTF-8   $REPORT_DIR/$DIR_YEAR/$DIR_MONTH/report.region.sj_buss.wireless_city.txt  > $REPORT_DIR/$DIR_YEAR/$DIR_MONTH/report.region.sj_buss_wireless_city.csv  ;
+./export_KPItmp.sh:iconv -f GB2312 -t UTF-8   $REPORT_DIR/$DIR_YEAR/$DIR_WEEK/report.region.sj_buss.wireless_city.txt  > $REPORT_DIR/$DIR_YEAR/$DIR_WEEK/report.region.sj_buss_wireless_city.csv ;
+./export_KPI.sh:iconv -f GB2312 -t UTF-8   $REPORT_DIR/$DIR_YEAR/$DIR_WEEK/report.region.sj_buss.wireless_city.txt  > $REPORT_DIR/$DIR_YEAR/$DIR_WEEK/report.region.sj_buss_wireless_city.csv ;
