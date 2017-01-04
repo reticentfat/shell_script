42a038129f7db65bf39c1b696887796f38daa7a1
reticentfat <reticentfat@users.noreply.github.com>
2017-01-04 17:48:12 +0800

    Update 全局搜索.sql

--- 全局搜索.sql
+++ 全局搜索.sql
@@ -1,3 +1,9 @@
+-n, --line-number
+在输出的每行前面加上它所在的文件中它的行号。
+-s, --no-messages
+禁止输出关于文件不存在或不可读的错误信息。
+-R, -r, --recursive
+递归地读每一目录下的所有文件。这样做和 -d recurse 选项等价。
 grep -nsr "outputumessage_003_meiti" shbb/
 oracle@wreport:/data/homeoracle/etl/bin$ find . -maxdepth 2 -name "*.sh" |xargs grep "report.region.sj_buss_wireless_city.csv"
 ./exportmonth_KPI.sh:iconv -f GB2312 -t UTF-8   $REPORT_DIR/$DIR_YEAR/$DIR_MONTH/report.region.sj_buss.wireless_city.txt  > $REPORT_DIR/$DIR_YEAR/$DIR_MONTH/report.region.sj_buss_wireless_city.csv  ;
