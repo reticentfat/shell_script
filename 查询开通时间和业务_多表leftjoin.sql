402de2c987563f06df3222a0eee1944e9ef062f3
reticentfat <reticentfat@users.noreply.github.com>
2017-02-27 09:39:44 +0800

    Update 查询开通时间和业务_多表leftjoin.sql

--- 查询开通时间和业务_多表leftjoin.sql
+++ 查询开通时间和业务_多表leftjoin.sql
@@ -2,4 +2,4 @@ select j.mobile, b.id, b.mobile_sub_time, o.opt_cost
   from jf_1 j
   left join bjwz b
     on j.mobile = b.mobile_sn
-  left join  opt_code_all o on 'F' || b.now_fee = o.opt_type
+  left join  opt_code_all o on b.opt_type = o.opt_type
