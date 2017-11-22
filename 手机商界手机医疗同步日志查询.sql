014130e51f78e7b9dab2a411c2519b93cc776c86
reticentfat <reticentfat@users.noreply.github.com>
2017-11-22 16:49:28 +0800

    Update 手机商界手机医疗同步日志查询.sql

--- 手机商界手机医疗同步日志查询.sql
+++ 手机商界手机医疗同步日志查询.sql
@@ -3,3 +3,12 @@
 马鹏飞  16:43:00
 7.1
 7.3为数据源/data/log/boss_log
+马鹏飞  16:39:31
+/data/log/sjyl_sjsj_rsync
+马鹏飞  16:43:00
+7.1
+------------------
+7.3为数据源/data/log/boss_log
+-rw-r--r-- 1 root     2714682 11 22 00:00 20171121_Boss_notify.log.bz2//异步
+-rw-r--r-- 1 root     7164859 11 22 00:00 20171121_Boss_read.log.bz2//处理异步
+-rw-r--r-- 1 root      683099 11 22 00:00 20171121_Boss_tran_to_wire.log.bz2//实时
