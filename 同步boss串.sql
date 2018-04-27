0d91d8bdc3910b21f3ac1c7b73ec17947ff43c83
reticentfat <reticentfat@users.noreply.github.com>
2018-04-27 14:22:18 +0800

    Create 同步boss串.sql

---
+++ 同步boss串.sql
@@ -0,0 +1,3 @@
+curl -H "Content-Type:application/json" -X POST -d '{"id":"1#####9","oprcode":"06","efftime":"19700101080000","spid":"801174","bizcode":"1258150","feetype":"2","source":"R1"}' http://192.100.#.###:7004/transaction
+短信业务
+curl -H "Content-Type:application/json" -X POST -d '{"id":"#","oprcode":"06","efftime":"19700101080000","spid":"901808","bizcode":"-UMGFLZS","feetype":"2","source":"R1"}' http://192.100.#.###:7004/transaction
